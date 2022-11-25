8fb38fc85693cc79024f844b9772f952a7085372
Anand <anand.tangri@velotio.com>
2022-11-25 12:41:24 +0530

    remove comments

--- server/delays.js
+++ server/delays.js
@@ -1,13 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-// Tweak these to play with different kinds of latency.
-
 // How long the data fetches on the server.
 exports.API_DELAY = 2000;
 
